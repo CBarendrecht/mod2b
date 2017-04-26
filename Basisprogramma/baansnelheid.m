87f265b066a8a4ded7d3d9aaf6899fab241fe500
Stijn <stijn.moerman@tele2.nl>
2017-04-26 17:24:35 +0200

    Eenheden: aardmassa's & AE

--- Basisprogramma/baansnelheid.m
+++ Basisprogramma/baansnelheid.m
@@ -1,9 +1,9 @@
 function [v] = baansnelheid(x)
     M = 1.99 * 10^30;
     G = 6.67 * 10^-11;
-    r =  sqrt(x(1)^2+x(2)^2);
-    v = zeros(2,1);
+    r =  sqrt(x(1)^2+x(2)^2)*1.5*10^11;
+    v = zeros(1,2);
     baanv = sqrt(G*M/r);
-    v(2) = x(1)/r * baanv;
-    v(1) = -x(2)/r * baanv;
+    v(2) = x(1)*1.5*10^11/r * baanv;
+    v(1) = -x(2)*1.5*10^11/r * baanv;
 end

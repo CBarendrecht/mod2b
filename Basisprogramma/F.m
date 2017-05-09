3bf444f4c63cb6ecb4ebb7075e1d201435cb447b
Stijn <stijn.moerman@tele2.nl>
2017-05-09 22:52:59 +0200

    Random planeten generator en kleine aanpasssingen

--- Basisprogramma/F.m
+++ Basisprogramma/F.m
@@ -3,7 +3,8 @@ function [y] = F(x,m,D)
     n = size(x);
     y = zeros(n);
     for i = 1:n(1)
-        y(i,1) = sum(G * m(D(i,:)> 0.1) .* ((x((D(i,:)> 0.1),1) - x(i,1)).*(D((D(i,:)> 0.1),i).^(-3/2))));
-        y(i,2) = sum(G * m(D(i,:)> 0.1) .* ((x((D(i,:)> 0.1),2) - x(i,2)).*(D((D(i,:)> 0.1),i).^(-3/2))));
+        vw = (D(i,:)> 0.01); %anders wordt de versnelling te groot
+        y(i,1) = sum(G * m(vw) .* ((x((vw),1) - x(i,1)).*(D((vw),i).^(-3/2))));
+        y(i,2) = sum(G * m(vw) .* ((x((vw),2) - x(i,2)).*(D((vw),i).^(-3/2))));
     end
 end
--- Basisprogramma/test.m
+++ Basisprogramma/test.m
@@ -1,15 +1,21 @@
 clear all; %we hebben tijd in maanden en afstand in AE
 M = 1.99 * 10^30/(5.97 * 10^24); %massa zon in aardmassa's
-n = 1 + 4; %aantal hemellichamen: zon + aantal planeten
-dt = 0.5; %tijdstapgrootte in maanden
-T = 300; %aantal tijdstappen
+n = 1 + 10; %aantal hemellichamen: zon + aantal planeten
+dt = 1; %tijdstapgrootte in maanden
+T = 500; %aantal tijdstappen
 
 A = zeros(5,n); %m,x,y,vx,vy
 A(:,1) = [M,0,0,0,0]; %zon
-A(:,2) = [1,-1.5,-1,baansnelheid([-1.5,-1])];
-A(:,3) = [50,0,1.08,baansnelheid([0,1.1])];
-A(:,4) = [50,1,0,baansnelheid([1,0])];
-A(:,5) = [150,-0.7,-0.71,baansnelheid([-0.7,-0.7])];
+%A(:,2) = [1,-1.5,-1,baansnelheid([-1.5,-1])];
+%A(:,3) = [50,0,1.08,baansnelheid([0,1.1])];
+%A(:,4) = [50,1,0,baansnelheid([1,0])];
+%A(:,5) = [150,-0.7,-0.71,baansnelheid([-0.7,-0.7])];
+
+for i = 2:n
+    b = 1.5*rand+0.2;
+    c = 2*pi*rand;
+    A(:,i) = [50,b*cos(c),b*sin(c),baansnelheid([b*cos(c),b*sin(c)])];
+end
 
 m = zeros(n,1); %massavector
 r = zeros(n,1); %straalvector
@@ -27,8 +33,12 @@ for i = 1:n
     v(i,1,2) = A(5,i); %snelheid y richting
 end
 for i = 1:n
-    for j = 1:n
-        D(i,j) = (x(i,1,1)-x(j,1,1))^2 + (x(i,1,2)-x(j,1,2))^2;
+    if m(i) > 0
+        for j = 1:n
+            if m(j) > 0
+                D(i,j) = (x(i,1,1)-x(j,1,1))^2 + (x(i,1,2)-x(j,1,2))^2;
+            end
+        end
     end
 end
 
@@ -40,13 +50,17 @@ axis([-2 2 -2 2]);
 hold on;
 pause(0.25);
 
-a = F(x((m>0),1,:),m,D); %versnelling op t = 0
+a = F(x((m>0),1,:),m(m>0),D((m > 0),(m > 0))); %versnelling op t = 0
 v(:,2,:) = v(:,1,:) + dt/2 * a; %snelheid op t = 1/2 dt
 x(:,2,:) = x(:,1,:) + v(:,2,:) * dt; %plaats op t = dt
 
 for i = 1:n
-    for j = 1:n
-        D(i,j) = (x(i,2,1)-x(j,2,1)).^2 + (x(i,2,2)-x(j,2,2)).^2;
+    if m(i) > 0
+        for j = 1:n
+            if m(j) > 0
+                D(i,j) = (x(i,2,1)-x(j,2,1)).^2 + (x(i,2,2)-x(j,2,2)).^2;
+            end
+        end
     end
 end
 
@@ -56,23 +70,23 @@ scatter(x((m>0 & m<10^5),2,1),x((m>0 & m<10^5),2,2),10^5*r(m>0 & m<10^5),[1,0,0]
 pause(0.25);
 
 for k = 3:T
-    a = F(x((m > 0),k-1,:),m,D((m > 0),(m > 0))); %versnelling op t = (k - 1) dt
-    v((m > 0),k,:) = v((m > 0),k-1,:) + a * dt; %snelheid op t = (k - 1/2) dt
+    a = F(x((m > 0),k-1,:),m(m>0),D((m > 0),(m > 0))); %versnelling op t = (k - 1) dt
+    v((m > 0),k,:) = v((m > 0),k-1,:) + a*dt; %snelheid op t = (k - 1/2) dt
     
     %botsen
     for i = 1:n-1
         for j = i+1:n
             if bots(x(i,k-1,:),x(j,k-1,:),v(i,k,:),v(j,k,:),dt,r(i),r(j))
-                if m(i)>m(j)
-                    v(i,k,:)=(m(i)*v(i,k,:)+m(j)*v(j,k,:))/(m(i)+m(j));
-                    m(i)= m(i)+m(j); %nog aanpassen
-                    x(i,k-1,:)=(x(i,k-1,:)+x(j,k-1,:))/2;
-                    m(j)=0;
+                if m(i) > m(j)
+                    v(i,k,:) = (m(i)*v(i,k,:) + m(j)*v(j,k,:))/(m(i)+m(j));
+                    m(i) = m(i) + m(j); %nog aanpassen
+                    x(i,k-1,:) = (x(i,k-1,:) + x(j,k-1,:))/2;
+                    m(j) = 0;
                 else
-                    v(j,k,:)=(m(j)*v(j,k,:)+m(i)*v(i,k,:))/(m(j)+m(i));
-                    m(j)= m(i)+m(j); %nog aanpassen
-                    x(j,k-1,:)=(x(j,k-1,:)+x(i,k-1,:))/2;
-                    m(i)=0;
+                    v(j,k,:) = (m(j)*v(j,k,:) + m(i)*v(i,k,:))/(m(j)+m(i));
+                    m(j)= m(i) + m(j); %nog aanpassen
+                    x(j,k-1,:) = (x(j,k-1,:) + x(i,k-1,:))/2;
+                    m(i) = 0;
                 end
                 r(i) = straal(m(i));
                 r(j) = straal(m(j));
@@ -82,8 +96,12 @@ for k = 3:T
     
     x((m > 0),k,:) = x((m > 0),k-1,:) + v((m > 0),k,:) * dt; %plaats op t = k dt
     for i = 1:n
-        for j = 1:n
-            D(i,j) = (x(i,k,1)-x(j,k,1))^2 + (x(i,k,2)-x(j,k,2))^2;
+        if m(i) > 0
+            for j = 1:n
+                if m(j) > 0
+                    D(i,j) = (x(i,k,1)-x(j,k,1))^2 + (x(i,k,2)-x(j,k,2))^2;
+                end
+            end
         end
     end
     

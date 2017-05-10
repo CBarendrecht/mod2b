0b9125b99793d45bd81bb71557968579e3e6f6e2
Stijn <stijn.moerman@tele2.nl>
2017-05-10 17:07:28 +0200

    Richardson extrapolatie

---
+++ Basisprogramma/Rextpol.m
@@ -0,0 +1,105 @@
+clear all; %we hebben tijd in maanden en afstand in AE
+%[p,dt,T,minR,maxR] = Menu(); %aantal planeten, tijdstap, aantal stappen en
+%min en max afstand tot zon
+for dt = [1 0.5 0.25]
+    p=1;
+    T=24/dt;
+    minR=1;
+    maxR=1;
+    n = 1 + p; %aantal hemellichamen: zon + aantal planeten
+    [m,r,x,v] = BigBang(n,T,minR,maxR); %aanmaken massavector, straalvector
+    %plaatsmatrix en snelheidsmatrix
+
+    D = zeros(n); %onderlinge afstandsmatrix
+    for i = 1:n
+        for j = 1:n
+            D(i,j) = (x(i,1,1)-x(j,1,1))^2 + (x(i,1,2)-x(j,1,2))^2;
+        end
+    end
+
+    figure;
+    scatter(x(1,1,1),x(1,1,2),10^4*r(1),[1,1,0],'filled');
+    hold on;
+    scatter(x((m>0 & m<10^5),1,1),x((m>0 & m<10^5),1,2),10^5*r(m>0 & m<10^5),[1,0,0],'filled');
+    axis([-2 2 -2 2]);
+    hold on;
+    pause(0.25);
+
+    a = F(x((m>0),1,:),m(m>0),D((m > 0),(m > 0))); %versnelling op t = 0
+    v(:,2,:) = v(:,1,:) + dt/2 * a; %snelheid op t = 1/2 dt
+    x(:,2,:) = x(:,1,:) + v(:,2,:) * dt; %plaats op t = dt
+
+    for i = 1:n
+        if m(i) > 0
+            for j = 1:n
+                if m(j) > 0
+                    D(i,j) = (x(i,2,1)-x(j,2,1)).^2 + (x(i,2,2)-x(j,2,2)).^2;
+                end
+            end
+        end
+    end
+
+    cla;
+    scatter(x(1,2,1),x(1,2,2),10^4*r(1),[1,1,0],'filled');
+    scatter(x((m>0 & m<10^5),2,1),x((m>0 & m<10^5),2,2),10^5*r(m>0 & m<10^5),[1,0,0],'filled');
+    pause(0.25);
+
+    for k = 3:T
+        a = F(x((m > 0),k-1,:),m(m>0),D((m > 0),(m > 0))); %versnelling op t = (k - 1) dt
+        v((m > 0),k,:) = v((m > 0),k-1,:) + a*dt; %snelheid op t = (k - 1/2) dt
+
+        %botsen
+        for i = 1:n-1
+            for j = i+1:n
+                if bots(x(i,k-1,:),x(j,k-1,:),v(i,k,:),v(j,k,:),dt,r(i),r(j))
+                    if m(i) > m(j)
+                        v(i,k,:) = (m(i)*v(i,k,:) + m(j)*v(j,k,:))/(m(i)+m(j));
+                        m(i) = m(i) + m(j); %nog aanpassen
+                        x(i,k-1,:) = (x(i,k-1,:) + x(j,k-1,:))/2;
+                        m(j) = 0;
+                    else
+                        v(j,k,:) = (m(j)*v(j,k,:) + m(i)*v(i,k,:))/(m(j)+m(i));
+                        m(j)= m(i) + m(j); %nog aanpassen
+                        x(j,k-1,:) = (x(j,k-1,:) + x(i,k-1,:))/2;
+                        m(i) = 0;
+                    end
+                    r(i) = straal(m(i));
+                    r(j) = straal(m(j));
+                end
+            end
+        end
+
+        x((m > 0),k,:) = x((m > 0),k-1,:) + v((m > 0),k,:) * dt; %plaats op t = k dt
+        for i = 1:n
+            if m(i) > 0
+                for j = 1:n
+                    if m(j) > 0
+                        D(i,j) = (x(i,k,1)-x(j,k,1))^2 + (x(i,k,2)-x(j,k,2))^2;
+                    end
+                end
+            end
+        end
+
+        cla;
+        scatter(x(1,k,1),x(1,k,2),10^4*r(1),[1,1,0],'filled');
+        scatter(x((m>0 & m<10^5),k,1),x((m>0 & m<10^5),k,2),10^5*r(m>0 & m<10^5),[1,0,0],'filled');
+        pause(0.25);
+    end
+    hold off;
+
+    figure;
+    for i = 1:n
+        plot(x(i,:,1),x(i,:,2));
+        hold on;
+    end
+    axis([-5 5 -5 5]);
+    
+    Qx(1/dt)=x(2,T,1);
+    Qy(1/dt)=x(2,T,2);
+end
+Q = atan(Qx./Qy);
+px = round(log((Qx(2)-Qx(1))/(Qx(4)-Qx(2)))/log(2));
+py = round(log((Qy(2)-Qy(1))/(Qy(4)-Qy(2)))/log(2));
+pp = round(log((Q(2)-Q(1))/(Q(4)-Q(2)))/log(2));
+
+

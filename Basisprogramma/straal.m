d259e31ece26146be159440341c9f35af9012876
LAPTOP-J9416U0R\Casper <casper.barendrecht@gmail.com>
2017-05-24 15:56:45 +0200

    straal?

--- Basisprogramma/straal.m
+++ Basisprogramma/straal.m
@@ -1,5 +1,7 @@
 function [r] = straal(M)
-    
-    rA = 6.37*10^6/(1.5*10^11);
-    r = rA * M^(1/3);
+    %dichtheid schalingsfactor 5.6533*10^8
+    rhos = 5.51*10^3 * 5.6533*10^8; %dichtheid aarde
+    rhog = 1.326*10^3 * 5.6533*10^8;   %dichteid jupiter
+    m = M(1)/rhos + M(2)/rhog;
+    r = (3*m/(4*pi))^(1/3);
 end

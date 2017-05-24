e4e483f219495c6d7ebabfe15405c15d0ad14b48
Stijn <stijn.moerman@tele2.nl>
2017-05-24 17:27:47 +0200

    Aanpassing

--- Basisprogramma/straal.m
+++ Basisprogramma/straal.m
@@ -1,7 +1,7 @@
 function [r] = straal(M)
     %dichtheid schalingsfactor 5.6533*10^8
     rhos = 5.51*10^3 * 5.6533*10^8; %dichtheid aarde
-    rhog = 1.326*10^3 * 5.6533*10^8;   %dichteid jupiter
+    rhog = 0.687*10^3 * 5.6533*10^8;   %dichteid saturnus
     m = M(1)/rhos + M(2)/rhog;
     r = (3*m/(4*pi))^(1/3);
 end

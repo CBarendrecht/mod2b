fcc4e8416c0297896d479ee2655e9f0a6efdeeba
Stijn <stijn.moerman@tele2.nl>
2017-05-29 15:54:07 +0200

    Grafieken AP, EINDM en EINDR voor 100 t/m 700

---
+++ Data/Uitgewerkte Data/EINDRgrafiek.m
@@ -0,0 +1,75 @@
+clear all;
+load('EINDM100.mat');
+EINDM100 = sum(EINDM,2);
+clear EINDM;
+load('EINDM200.mat');
+EINDM200 = sum(EINDM,2);
+clear EINDM;
+load('EINDM300.mat');
+EINDM300 = sum(EINDM,2);
+clear EINDM;
+load('EINDM400.mat');
+EINDM400 = sum(EINDM,2);
+clear EINDM;
+load('EINDM500.mat');
+EINDM500 = sum(EINDM,2);
+clear EINDM;
+load('EINDM600.mat');
+EINDM600 = sum(EINDM,2);
+clear EINDM;
+load('EINDM700.mat');
+EINDM700 = sum(EINDM,2);
+clear EINDM;
+
+load('EINDR100.mat');
+EINDR100 = EINDR;
+clear EINDR;
+load('EINDR200.mat');
+EINDR200 = EINDR;
+clear EINDR;
+load('EINDR300.mat');
+EINDR300 = EINDR;
+clear EINDR;
+load('EINDR400.mat');
+EINDR400 = EINDR;
+clear EINDR;
+load('EINDR500.mat');
+EINDR500 = EINDR;
+clear EINDR;
+load('EINDR600.mat');
+EINDR600 = EINDR;
+clear EINDR;
+load('EINDR700.mat');
+EINDR700 = EINDR;
+clear EINDR;
+
+RP100 = zeros(5,1);
+RP200 = zeros(5,1);
+RP300 = zeros(5,1);
+RP400 = zeros(5,1);
+RP500 = zeros(5,1);
+RP600 = zeros(5,1);
+RP700 = zeros(5,1);
+
+for i = 1:5
+    RP100(i) = mean(EINDR100(EINDM100(:,i) > 0.06,i));
+    RP200(i) = mean(EINDR200(EINDM200(:,i) > 0.06,i));
+    RP300(i) = mean(EINDR300(EINDM300(:,i) > 0.06,i));
+    RP400(i) = mean(EINDR400(EINDM400(:,i) > 0.06,i));
+    RP500(i) = mean(EINDR500(EINDM500(:,i) > 0.06,i));
+    RP600(i) = mean(EINDR600(EINDM600(:,i) > 0.06,i));
+    RP700(i) = mean(EINDR700(EINDM700(:,i) > 0.06,i));
+end
+MRP(1) = mean(RP100);
+MRP(2) = mean(RP200);
+MRP(3) = mean(RP300);
+MRP(4) = mean(RP400);
+MRP(5) = mean(RP500);
+MRP(6) = mean(RP600);
+MRP(7) = mean(RP700);
+
+figure;
+scatter(100:100:700,MRP*1.5*10^8/(6.371*10^3),25,[0,0,1],'o','filled');
+title('Mean radius of planets after 5000 years');
+xlabel('Initial number of celestial bodies');
+ylabel('Mean radius in earth radius');

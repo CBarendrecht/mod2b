db8a0039d6ba62be78f3cdac3109dd0af2b5370b
LAPTOP-J9416U0R\Casper <casper.barendrecht@gmail.com>
2017-06-07 16:07:04 +0200

    botsfunctie aangepast

--- Basisprogramma/bots.m
+++ Basisprogramma/bots.m
@@ -2,12 +2,11 @@ function Bam = bots(r1,r2,v1,v2,dt,R1,R2)
     Bam = false;
     Dv = v1(1,:)' - v2(1,:)';
     Dr = r1(1,:)' - r2(1,:)';
-    t = -Dv'*Dr;% minder delen meer beter
+    t = (-Dv'*Dr)/(Dv'*Dv); %minimale afstand
 
-    if 0 <= t && t <= dt*(Dv'*Dv)
-        if (Dr*(Dv'*Dv)+Dv*t)'*(Dr*(Dv'*Dv)+Dv*t) < (Dv'*Dv)*(R1+R2)^2
+    if 0 <= t && t <= dt
+        if (Dr+Dv*t)'*(Dr+Dv*t) <(R1+R2)^2
             Bam = true;
         end
     end
-   
 end

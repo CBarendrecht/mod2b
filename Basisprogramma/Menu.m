2f105a6634835e1e06b8783292732943ace897aa
goldsear <n.j.b.snijder@umail.leidenuniv.nl>
2017-06-11 11:05:59 +0200

    Nand Simulaties: p=1300 tot 5000 jaar, foutje in Menu gefixt

--- Basisprogramma/Menu.m
+++ Basisprogramma/Menu.m
@@ -9,13 +9,13 @@ function [p, dt, T, minR, maxR, minM, maxM] = Menu()
 
     dlg_title = 'Input';
     num_lines = 1;
-    defaultans = {'1000', '1','20000', '1', '7','0.001','0.01'} ;
+    defaultans = {'1000', '1','20000', '1', '7','0.01','0.02'} ;
     options.Resize = 'on';
     answer = inputdlg(prompt,dlg_title,num_lines,defaultans,options);
 
     p = str2num(answer{1});
     dt = str2num(answer{2});
-    T = 12*str2num(answer{3});
+    T = 12/dt*str2num(answer{3});
     minR = str2num(answer{4});
     maxR = str2num(answer{5});
     minM = str2num(answer{6});
